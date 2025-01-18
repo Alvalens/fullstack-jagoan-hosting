482011fad43737881a296f6b906eeaf5e5d52089
Alvalen <alvalen.shafel04@gmail.com>
2025-01-19 06:01:48 +0700

    chore: add environment configuration

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,11 +1,13 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge"
 
+const AssetsBaseUrl =
+	import.meta.env.VITE_ASSET_BASE_URL ?? "http://127.0.0.1:8000/storage/";
+
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
-
 export function showImage(image) {
-  return `http://127.0.0.1:8000/storage/${image}`;
+  return `${AssetsBaseUrl}${image}`;
 }
